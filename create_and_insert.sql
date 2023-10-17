aeeed6b529feeea4bc00fa375bf5e1cf7fc4479d
MyWahyuSap <wahyusaputra222000@gmail.com>
2023-10-17 22:46:43 +0700

    17/10/2023 final

--- create_and_insert.sql
+++ create_and_insert.sql
@@ -1,6 +1,6 @@
 -- create table Nasabah
 CREATE TABLE IF NOT EXISTS nasabah (
-    Id_Nasabah SERIAL PRIMARY KEY,
+    Id SERIAL PRIMARY KEY,
     Nama VARCHAR(255) NOT NULL,
     Alamat VARCHAR(255) NOT NULL,
     Nomor_Telepon VARCHAR(20) NOT NULL,
@@ -13,11 +13,11 @@ CREATE TABLE IF NOT EXISTS nasabah (
 
 -- create table Akun
 CREATE TABLE IF NOT EXISTS akun (
-    Id_Akun SERIAL PRIMARY KEY,
+    Id SERIAL PRIMARY KEY,
     Id_Nasabah INT NOT NULL,
     Jenis_Akun VARCHAR(50) NOT NULL,
     Saldo DECIMAL(15, 2) NOT NULL,
-    FOREIGN KEY (ID_Nasabah) REFERENCES Nasabah(ID_Nasabah),
+    FOREIGN KEY (ID_Nasabah) REFERENCES Nasabah(Id),
     CreatedAt TIMESTAMP NOT NULL DEFAULT NOW(),
     UpdatedAt TIMESTAMP DEFAULT NOW(),
     DeletedAt TIMESTAMP
@@ -25,60 +25,119 @@ CREATE TABLE IF NOT EXISTS akun (
 
 -- create table Transaksi
 CREATE TABLE IF NOT EXISTS transaksi (
-    Id_Transaksi SERIAL PRIMARY KEY,
+    Id SERIAL PRIMARY KEY,
     Id_Akun INT NOT NULL,
     Tanggal_Transaksi DATE NOT NULL,
-    Jumlah_Transaksi DECIMAL(15, 2) NOT NULL,
     Keterangan VARCHAR(255),
-    FOREIGN KEY (ID_Akun) REFERENCES Akun(ID_Akun),
+    FOREIGN KEY (ID_Akun) REFERENCES Akun(Id),
     CreatedAt TIMESTAMP NOT NULL DEFAULT NOW(),
     UpdatedAt TIMESTAMP DEFAULT NOW(),
     DeletedAt TIMESTAMP
 );
 
--- Create
+-- CRUD nasabah
+-- CREATE
 -- Menambahkan nasabah baru
 INSERT INTO nasabah (Nama, Alamat, Nomor_Telepon, Email, Tanggal_Lahir)
 VALUES ('Saputra', 'Jl. Contoh No. 123', '081234567890', 'saputra@gmail.com', '2002-02-22'),
 ('Wahyu', 'Jl. Baru No. 456', '081234567890', 'wahyu@gmail.com', '2002-03-22'),
-('Budi', 'Jl. Baru No. 789', '081234567890', 'budi@gmail.com', '2002-04-22');
+('Budi', 'Jl. Baru No. 789', '081234567890', 'budi@gmail.com', '2002-04-22'),
+('Rudi', 'Jl. Baru No. 987', '081234567890', 'rudi@gmail.com', '2002-05-22'),
+('Andi', 'Jl. Baru No. 123', '081234567890', 'andi@gmail.com', '2002-06-22');
 
--- Menambahkan akun baru untuk nasabah
-INSERT INTO akun (Id_Nasabah, Jenis_Akun, Saldo)
-VALUES (1, 'Tabungan', 5000.00),
-(2, 'Tabungan', 6000.00),
-(3, 'Tabungan', 7000.00);
+-- Indexing
+CREATE INDEX idx_nasabah ON nasabah (Nama);
 
--- Mencatat transaksi
-INSERT INTO transaksi (Id_Akun, Tanggal_Transaksi, Jumlah_Transaksi, Keterangan)
-VALUES (1, '2023-10-16', 1000.00, 'Setoran'),
-(2, '2023-10-16', 2000.00, 'Setoran'),
-(3, '2023-10-16', 3000.00, 'Setoran');
-
--- Read
--- Membaca data nasabah
+-- READ
+-- Menampilkan seluruh data nasabah
 SELECT * FROM nasabah;
 
--- Membaca data akun untuk seorang nasabah tertentu (misalnya, nasabah dengan ID_Nasabah=1)
-SELECT * FROM akun WHERE Id_Nasabah = 1;
-
--- Membaca data transaksi untuk sebuah akun tertentu (misalnya, akun dengan ID_Akun=101)
-SELECT * FROM transaksi WHERE Id_Akun = 1;
-
--- Update
+-- UPDATE
 -- Mengubah alamat seorang nasabah
 UPDATE nasabah
 SET Alamat = 'Jl. Baru No. 123'
-WHERE Id_Nasabah = 1;
+WHERE Id = 1;
 
--- Mengupdate saldo akun setelah transaksi
-UPDATE akun
-SET Saldo = Saldo + 500.00
-WHERE Id_Akun = 1;
+-- DELETE
+-- Menghapus nasabah
+DELETE FROM nasabah WHERE Id = 1;
 
--- Delete
--- Menghapus nasabah dan semua akun yang terkait dengannya
-DELETE FROM nasabah WHERE Id_Nasabah = 1;
+-- CRUD akun
+-- CREATE
+-- Menambahkan akun baru untuk nasabah
+INSERT INTO akun (Id_Nasabah, Jenis_Akun, Saldo)
+VALUES (2, 'Tabungan', 6000.00),
+(3, 'Tabungan', 7000.00),
+(4, 'Tabungan', 8000.00),
+(5, 'Tabungan', 9000.00);
 
--- Menghapus transaksi tertentu
-DELETE FROM transaksi WHERE Id_Transaksi = 1;
+-- Indexing
+CREATE INDEX idx_akun ON akun (Jenis_Akun);
+
+-- READ
+-- Membaca data nasabah dan data akun
+SELECT * FROM nasabah INNER JOIN akun ON nasabah.Id = akun.Id_Nasabah;
+
+-- UPDATE
+CREATE OR REPLACE PROCEDURE transfer (
+    idPengirim INT,
+    idPenerima INT,
+    nominal DECIMAL(15, 2)
+)
+LANGUAGE plpgsql
+as $$
+BEGIN
+    -- Mengupdate saldo akun pengirim
+    UPDATE akun
+    SET Saldo = Saldo - nominal
+    WHERE Id_Akun = idPengirim;
+    -- Mengupdate saldo akun penerima
+    UPDATE akun
+    SET Saldo = Saldo + nominal
+    WHERE Id_Akun = idPenerima;
+
+    COMMIT;
+END;
+$$;
+
+-- transfer dari id_akun 2 ke id_akun 3
+CALL transfer(2, 3, 1000.00);
+
+-- QUERY CTE
+WITH nasabah_saldo AS (
+    SELECT nama,saldo
+    FROM nasabah INNER JOIN akun
+    ON nasabah.Id = akun.Id_Nasabah;
+)
+
+SELECT nama, saldo FROM nasabah_saldo
+ORDER BY nama ASC;
+
+-- DELETE
+-- Menghapus akun
+DELETE FROM akun WHERE Id = 5;
+
+-- CRUD transaksi
+-- CREATE
+-- Menambahkan transaksi baru
+INSERT INTO transaksi (Id_Akun, Tanggal_Transaksi, Keterangan)
+VALUES(2, '2023-10-16', 'Belanja'),
+(3, '2023-10-16', 'Menabung'),
+(4, '2023-10-16', 'Setoran');
+
+-- Indexing
+CREATE INDEX idx_transaksi ON transaksi (Keterangan);
+
+-- READ
+-- Membaca data transaksi
+SELECT * FROM transaksi WHERE Id_Akun = 3;
+
+-- UPDATE
+-- Mengubah keterangan transaksi
+UPDATE transaksi
+SET Keterangan = 'Setoran dari tabungan'
+WHERE Id_Transaksi = 3;
+
+-- DELETE
+-- Menghapus transaksi
+DELETE FROM transaksi WHERE Id = 4;
